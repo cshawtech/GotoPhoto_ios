f5cb0d8db5e8df58dc4f88e0fc16485ac23f01ed
Chris Shaw <chrisshaw@chriss-macbook-pro.local>
2018-12-21 23:30:49 +1100

    Initial Commit

---
+++ GotoPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GotoPhoto
+//
+//  Created by Chris Shaw on 21/12/18.
+//  Copyright © 2018 Chris Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
